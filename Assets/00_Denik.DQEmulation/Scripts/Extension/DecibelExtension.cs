b8457ea77b865969534f3720876fd7daa21b3853
xrdnk <xrdnk1010@gmail.com>
2021-04-08 23:10:04 +0900

    feat: float ⇆ db 変換拡張メソッド

---
+++ Assets/00_Denik.DQEmulation/Scripts/Extension/DecibelExtension.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Denik.DQEmulation.Extension
+{
+    public static class DecibelExtension
+    {
+        private static readonly float kMinDecibel = 80f;
+
+        public static float DecibelToFloat(float decibelValue)
+        {
+            var floatValue = (decibelValue + kMinDecibel) / kMinDecibel;
+            return floatValue * floatValue;
+        }
+
+        public static float FloatToDecibel(float floatValue)
+        {
+            floatValue = Mathf.Sqrt(floatValue);
+            var decibelValue = floatValue * kMinDecibel - kMinDecibel;
+            return decibelValue;
+        }
+    }
+}
