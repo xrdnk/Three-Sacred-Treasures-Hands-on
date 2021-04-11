31e9e71a51ddd50e5db4c4f2b5a41fb045e1d6c9
xrdnk <xrdnk1010@gmail.com>
2021-04-11 14:33:30 +0900

    refactor: DI interface

--- Assets/DQEmulation/Scripts/01_Presentation/Presenter/DQEmulationPresenter.cs
+++ Assets/DQEmulation/Scripts/01_Presentation/Presenter/DQEmulationPresenter.cs
@@ -8,10 +8,10 @@ namespace Denik.DQEmulation.Presenter
 {
     public class DQEmulationPresenter : Zenject.IInitializable, VContainer.Unity.IPostStartable, IDisposable
     {
-        private PlayerModel _playerModel;
-        private EnemyModel _enemyModel;
-        private PlayerView _playerView;
-        private EnemyView _enemyView;
+        private IPlayer _player;
+        private IEnemy _enemy;
+        private IPlayerView _playerView;
+        private IEnemyView _enemyView;
         private BGMPlayer _bgmPlayer;
 
         private readonly CompositeDisposable _compositeDisposable = new CompositeDisposable();
@@ -19,13 +19,13 @@ namespace Denik.DQEmulation.Presenter
         [VContainer.Inject]
         [Zenject.Inject]
         private void Construct(
-            PlayerModel playerModel, EnemyModel enemyModel,
-            PlayerView playerView, EnemyView enemyView,
+            IPlayer playerModel, IEnemy enemyModel,
+            IPlayerView playerView, IEnemyView enemyView,
             BGMPlayer bgmPlayer
             )
         {
-            _playerModel = playerModel;
-            _enemyModel = enemyModel;
+            _player = playerModel;
+            _enemy = enemyModel;
             _playerView = playerView;
             _enemyView = enemyView;
             _bgmPlayer = bgmPlayer;
@@ -52,58 +52,58 @@ namespace Denik.DQEmulation.Presenter
         {
             // Enemy Model -> Views
             // 敵の名前をViewに表示する
-            _enemyView.DisplayName(_enemyModel.Name);
+            _enemyView.DisplayName(_enemy.Name);
             // 敵の姿をViewに表示する
-            _enemyView.DisplayFigure(_enemyModel.Figure);
+            _enemyView.DisplayFigure(_enemy.Figure);
             // 敵のHPをViewに表示する
-            _enemyModel.HitPoint
+            _enemy.HitPoint
                 .Subscribe(_enemyView.DisplayHp)
                 .AddTo(_compositeDisposable);
             // 敵が攻撃を受けたことをViewに通知する
-            _enemyModel.OnDamagedAsObservable()
+            _enemy.OnDamagedAsObservable()
                 .Subscribe(damagedPoint => _enemyView.DisplayDamaged(damagedPoint))
                 .AddTo(_compositeDisposable);
             // 敵が殺されたことをViewに通知する
-            _enemyModel.OnDiedAsObservable()
+            _enemy.OnDiedAsObservable()
                 .Subscribe(attackerName => _enemyView.DisplayDied(attackerName))
                 .AddTo(_compositeDisposable);
 
             // Player Model -> Views
             // プレイヤーの名前をViewに表示する
-            _playerView.DisplayName(_playerModel.Name);
+            _playerView.DisplayName(_player.Name);
             // プレイヤーの姿をViewに表示する
-            _playerView.DisplayFigure(_playerModel.Figure);
+            _playerView.DisplayFigure(_player.Figure);
             // プレイヤーのHPをViewに表示する
-            _playerModel.HitPoint
+            _player.HitPoint
                 .Subscribe(_playerView.DisplayHp)
                 .AddTo(_compositeDisposable);
             // プレイヤーが攻撃されたことをViewに通知する
-            _playerModel.OnDamagedAsObservable()
+            _player.OnDamagedAsObservable()
                 .Subscribe(damagedPoint => _playerView.DisplayDamaged(damagedPoint))
                 .AddTo(_compositeDisposable);
             // プレイヤーが死んだことをViewに通知する
-            _playerModel.OnDiedAsObservable()
+            _player.OnDiedAsObservable()
                 .Subscribe(attackerName => _playerView.DisplayDied(attackerName))
                 .AddTo(_compositeDisposable);
             // プレイヤーが回復したことをViewに通知する
-            _playerModel.OnHealedAsObservable()
+            _player.OnHealedAsObservable()
                 .Subscribe(healedPoint => _playerView.DisplayHealed(healedPoint))
                 .AddTo(_compositeDisposable);
 
             // Enemy View -> Models
             // 敵が攻撃したことをModelに通知する
             _enemyView.OnAttackTriggerAsObservable()
-                .Subscribe(attackerName => _playerModel.TakeDamage(attackerName, _enemyModel.AttackPower))
+                .Subscribe(attackerName => _player.TakeDamage(attackerName, _enemy.AttackPower))
                 .AddTo(_compositeDisposable);
 
             // Player View -> Models
             // プレイヤーが攻撃したことを通知する
             _playerView.OnAttackTriggerAsObservable()
-                .Subscribe(attackerName => _enemyModel.TakeDamage(attackerName, _playerModel.AttackPower))
+                .Subscribe(attackerName => _enemy.TakeDamage(attackerName, _player.AttackPower))
                 .AddTo(_compositeDisposable);
             // プレイヤーが回復魔法を唱えたことを通知する
             _playerView.OnHealTriggerAsObservable()
-                .Subscribe(_ => _playerModel.Heal(_playerModel.HealPower))
+                .Subscribe(_ => _player.Heal(_player.HealPower))
                 .AddTo(_compositeDisposable);
 
             // 今回は一時的にここにBGM再生処理を行う
