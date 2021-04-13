b614161f92889a94086aebaa98bc3de7ae3661f6
xrdnk <xrdnk1010@gmail.com>
2021-04-13 10:50:57 +0900

    feat: VContainerのRegister設定の更新

--- Assets/DQEmulation/Scripts/98_DIContainer/LifetimeScope/DQEmulationLifetimeScope.cs
+++ Assets/DQEmulation/Scripts/98_DIContainer/LifetimeScope/DQEmulationLifetimeScope.cs
@@ -2,6 +2,7 @@ using Denik.DQEmulation.Entity;
 using Denik.DQEmulation.Model;
 using Denik.DQEmulation.Presenter;
 using Denik.DQEmulation.Repository;
+using Denik.DQEmulation.Service;
 using Denik.DQEmulation.View;
 using UnityEngine;
 using VContainer;
@@ -16,6 +17,8 @@ namespace Denik.DQEmulation.LifetimeScopes
         [SerializeField]
         private EnemyData enemyData = default;
         [SerializeField]
+        private BGMData _bgmData = default;
+        [SerializeField]
         private PlayerModel _playerModel = default;
         [SerializeField]
         private EnemyModel _enemyModel = default;
@@ -23,27 +26,38 @@ namespace Denik.DQEmulation.LifetimeScopes
         private PlayerView _playerView = default;
         [SerializeField]
         private EnemyView _enemyView = default;
+        [SerializeField]
+        private BGMPlayer _bgmPlayer = default;
+        [SerializeField]
+        private PlayerSettingsView _playerSettingsView = default;
 
         protected override void Configure(IContainerBuilder builder)
         {
             // Entity
             builder.RegisterInstance(enemyData);
             builder.RegisterInstance(playerData);
+            builder.RegisterInstance(_bgmData);
 
-            // Resource Provider
-            builder.Register<EnemyRepository>(Lifetime.Scoped);
-            builder.Register<PlayerRepository>(Lifetime.Scoped);
+            // Repository
+            builder.Register<EnemyRepository>(Lifetime.Scoped).AsImplementedInterfaces().AsSelf();
+            builder.Register<PlayerRepository>(Lifetime.Scoped).AsImplementedInterfaces().AsSelf();
+            builder.Register<BGMRepository>(Lifetime.Scoped).AsImplementedInterfaces().AsSelf();
 
             // Model
-            builder.RegisterComponent(_enemyModel).AsImplementedInterfaces().AsSelf();
-            builder.RegisterComponent(_playerModel).AsImplementedInterfaces().AsSelf();
+            builder.RegisterInstance(_enemyModel).AsImplementedInterfaces().AsSelf();
+            builder.RegisterInstance(_playerModel).AsImplementedInterfaces().AsSelf();
+
+            // UseCase
+            builder.RegisterInstance(_bgmPlayer).AsImplementedInterfaces().AsSelf();
 
             // View
             builder.RegisterComponent(_enemyView).AsImplementedInterfaces().AsSelf();
             builder.RegisterComponent(_playerView).AsImplementedInterfaces().AsSelf();
+            builder.RegisterComponent(_playerSettingsView).AsSelf();
 
             // Presenter (EntryPoint)
             builder.RegisterEntryPoint<DQEmulationPresenter>(Lifetime.Scoped);
+            builder.RegisterEntryPoint<PlayerSettingsPresenter>(Lifetime.Scoped);
         }
     }
 }
